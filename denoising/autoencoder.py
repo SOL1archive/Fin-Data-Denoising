253f5bd257cf71b5a3fa9cb4ef782d0480336f94
SOL1archive <enlightkorean@gmail.com>
2023-02-25 00:56:07 +0900

    try:autoencoder models

--- denoising/autoencoder.py
+++ denoising/autoencoder.py
@@ -2,5 +2,27 @@ import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
-    def __init__(self, fc2):
-        super().__init__()
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+        self.encoder_cnn = nn.Sequential(
+            nn.Conv1d(1, 8, (3, 4), stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(8, 16, (3, 4), stride=1, padding=1),
+            nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 32, (3, 4), stride=1, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.flatten = nn.Flatten(start_dim=1)
+
+        self.encoder_ffn = nn.Sequential(
+            nn.Linear(10, 20)
+        )
+
+    def forward(self, x):
+        x = self.encoder_cnn(x)
+        x = self.flatten(x)
+        print(x.shape)
+        return x
