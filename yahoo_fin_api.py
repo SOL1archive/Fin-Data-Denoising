605c6881b2117ad01dd0f0c71ce0722d9e125327
SOL1archive <enlightkorean@gmail.com>
2023-02-24 15:16:01 +0900

    add:load_df

--- yahoo_fin_api.py
+++ yahoo_fin_api.py
@@ -2,11 +2,25 @@ import yaml
 import pandas as pd
 import yfinance as yf
 
-def download():
-    option = yaml.load('data-load.yaml')
-    data_df = yf.download(**option)
+def download(option=None):
+    if option is None:
+        option = yaml.load('data-load.yaml')
+    df = yf.download(**option)
 
-    data_df.to_csv(option['ticker'] + option['start'] + option['end'] + option['interval'] + '.csv')
+    df.to_csv(option['ticker'] + option['start'] + option['end'] + option['interval'] + '.csv')
+
+    return df
+
+def load_df(filepath_or_buffer=None):
+    if filepath_or_buffer is None:
+        option = yaml.load('data-load.yaml')
+        df = pd.read_csv(option['ticker'] + option['start'] + option['end'] + option['interval'] + '.csv')
+    else:
+        df = pd.read_csv(filepath_or_buffer).set_index('Date')
+
+    return df
 
 if __name__ == '__main__':
-    download()
+    df = download()
+    print(df.head())
+
