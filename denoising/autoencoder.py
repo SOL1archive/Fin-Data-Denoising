f12c4df47b43c6eff9a2ad83a02f3e76a3432bcc
SOL1archive <enlightkorean@gmail.com>
2023-02-27 01:56:41 +0900

    add:hyperparamter tuning, add:lstm

--- denoising/autoencoder.py
+++ denoising/autoencoder.py
@@ -1,17 +1,31 @@
 import torch
 import torch.nn as nn
 
+# Hyperparameters
+## CNN
+
+## LSTM
+
+
+# CNN
+
 class CnnEncoder(nn.Module):
-    def __init__(self, encoded_space_dim):
+    def __init__(self, 
+                 conv1_kernel=3, 
+                 conv2_kernel=3, 
+                 conv3_kernel=3, 
+                 encoded_space_dim=0
+                 ):
         super(CnnEncoder, self).__init__()
 
         self.encoder_cnn = nn.Sequential(
-            nn.Conv1d(1, 8, 3, stride=1),
+            nn.Conv1d(1, 8, conv1_kernel, stride=1),
             nn.ReLU(inplace=True),
-            nn.Conv1d(8, 16, 3, stride=1),
+            nn.Conv1d(8, 16, conv2_kernel, stride=1),
+        
             #nn.BatchNorm1d(16),
             nn.ReLU(inplace=True),
-            nn.Conv1d(16, 32, 3, stride=1),
+            nn.Conv1d(16, 32, conv3_kernel, stride=1),
             nn.ReLU(inplace=True)
         )
 
@@ -20,18 +34,32 @@ class CnnEncoder(nn.Module):
         return x
 
 class CnnDecoder(nn.Module):
-    def __init__(self, encoded_space_dim) -> None:
+    def __init__(self, 
+                 conv1_kernel=3, 
+                 conv2_kernel=3, 
+                 conv3_kernel=3, 
+                 encoded_space_dim=0
+                 ):
         super(CnnDecoder, self).__init__()
 
         self.decoder_cnn = nn.Sequential(
-            nn.ConvTranspose1d(32, 16, 3, stride=1),
+            nn.ConvTranspose1d(32, 16, conv3_kernel, stride=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose1d(16, 8, 3, stride=1),
+            nn.ConvTranspose1d(16, 8, conv2_kernel, stride=1),
             nn.ReLU(inplace=True),
-            nn.ConvTranspose1d(8, 1, 3, stride=1)
+            nn.ConvTranspose1d(8, 1, conv1_kernel, stride=1)
         )
 
     def forward(self, x):
         x = self.decoder_cnn(x)
         return x
-        
+
+# LSTM
+
+class LstmEncoder(nn.Module):
+    def __init__(self, encoded_space_dim) -> None:
+        super(LstmEncoder).__init__()
+
+        self.encoder_lstm = nn.Sequential(
+            nn.LSTM(),
+        )
