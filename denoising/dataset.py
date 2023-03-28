c4d2b6dba5944c289ca1a3221c0ea2aaa6580f84
SOL1archive <enlightkorean@gmail.com>
2023-03-29 01:11:57 +0900

    update:padding in dataset

--- denoising/dataset.py
+++ denoising/dataset.py
@@ -1,13 +1,18 @@
 import torch
+from torch.nn.functional import pad
 from torch.utils.data import Dataset
 
 class SeqWindowDataset(Dataset):
     def __init__(self, data, window_size):
         self.data = data
+        #self.pad_value = data.mean()
+        self.pad_value = 0
         self.window_size = window_size
 
     def __len__(self):
         return len(self.data) - self.window_size + 1
 
     def __getitem__(self, idx):
-        return self.data[idx:idx+self.window_size]
+        seq = self.data[idx:idx+self.window_size]
+        
+        return pad(seq, self.window_size, value=self.pad_value)
